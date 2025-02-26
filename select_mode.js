63d9d98ce051fdfec0d1d398cc532a84ea84e65c
cheeze37 <poker97.123.321.111.223@gmail.com>
2025-02-26 10:43:43 +0900

    select_mode.css 디자인 수정

--- select_mode.js
+++ select_mode.js
@@ -4,7 +4,7 @@ let selectedAiCount = null;
 // 난이도 선택
 function selectDifficulty(difficulty, element) {
     selectedDifficulty = difficulty;
-    document.getElementById("selected-difficulty").innerText = "선택된 난이도: " + difficulty;
+    document.getElementById("selected-difficulty").innerText =  difficulty;
 
     // 기존 선택된 버튼 스타일 제거 후 새 버튼에 추가
     document.querySelectorAll(".button-container button").forEach(btn => btn.classList.remove("selected"));
@@ -15,7 +15,7 @@ function selectDifficulty(difficulty, element) {
 // AI 플레이어 수 선택
 function selectAiCount(aiCount, element) {
     selectedAiCount = aiCount;
-    document.getElementById("selected-ai").innerText = "선택된 AI 수: " + aiCount + "명";
+    document.getElementById("selected-ai").innerText = aiCount + "명";
 
     document.querySelectorAll(".button-container button").forEach(btn => btn.classList.remove("selected"));
     element.classList.add("selected");
